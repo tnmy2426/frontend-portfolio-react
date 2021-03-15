f0ceab1b67311a90e345a1d16499b1a7ff0fe7e5
Abdulla Al Nahdi <al.nahdi.tnmy@gmail.com>
2021-03-16 03:56:01 +0600

    Main hero section design done and icon added and some color changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.scss";
 import NavBar from "./Components/NavBar";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <div className='App'>
-      <div className='sidebar'>
+      <aside className='sidebar'>
         <NavBar />
-      </div>
-      <div className='main_content'></div>
+      </aside>
+      <main className='main_content'>
+        <div className='content'>
+          <HomePage />
+        </div>
+      </main>
     </div>
   );
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,40 @@
+import {
+  faFacebookF,
+  faGithub,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { Link } from "react-router-dom";
 import React from "react";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <header className='HomePage'>
+      <div className='hero'>
+        <h1 className='hero_text'>
+          Hello, I am <span>Abdullah Al Nahdi</span>
+        </h1>
+        <h2>Full-Stack Developer</h2>
+        <p>React | Python | Django</p>
+      </div>
+      <div className='social_icons'>
+        <Link to='https://www.linkedin.com/in/abdullah-al-nahdi-335a1a12a/'>
+          <FontAwesomeIcon icon={faLinkedinIn} className='linkedin' />
+        </Link>
+        <Link to='https://github.com/tnmy2426'>
+          <FontAwesomeIcon icon={faGithub} className='github' />
+        </Link>
+        <Link to='https://twitter.com/tnmy2426'>
+          <FontAwesomeIcon icon={faTwitter} className='twitter' />
+        </Link>
+        <Link to='https://www.facebook.com/xdxtnmy/'>
+          <FontAwesomeIcon icon={faFacebookF} className='facebook' />
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default HomePage;
